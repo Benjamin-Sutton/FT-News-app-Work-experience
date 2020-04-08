098be7e0ba54b1bcaf29e504170f6a4829648ad5
Ben Sutton <52448940+Benjamin-Sutton@users.noreply.github.com>
2020-04-08 09:57:13 +0100

    html now rendering

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const bodyParser = require ('body-parser')
 const app = express()
 const port = 3000
+const path = require('path')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set('view engine', 'html')
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/main.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+
